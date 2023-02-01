e2cfad1f2e0632b48de027be9d9a8b9339e88f86
TJ Hunter <tj@pronto.io>
2023-01-31 18:16:22 -0700

    JSON RPC stuff

--- Test Project/Test_ProjectApp.swift
+++ Test Project/Test_ProjectApp.swift
@@ -6,11 +6,30 @@
 //
 
 import SwiftUI
+import Foundation
+import JsonRPC
+import Serializable
 
 @main
 struct Test_ProjectApp: App {
     let persistenceController = PersistenceController.shared
 
+    init() {
+        print("hello!")
+//        let rpc = JsonRpc(.ws(url: URL(string: "ws://ender3.local/websocket")!), queue: .main)
+//
+//        rpc.call(method: "server.connection.identify", params: Params(), String.self, String.self) { res in
+//            print("back from command")
+//            print(try! res.get())
+//        }
+        
+        Task {
+            let rpc = JsonRpc(.ws(url: URL(string: "ws://ender3.local/websocket")!), queue: .main)
+            let res = try await rpc.call(method: "server.connection.identify", params: Params(), String.self, String.self)
+            print(res)
+        }
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
